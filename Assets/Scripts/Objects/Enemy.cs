77315b147d55bb9cae8e1f9ec6b7f0a8aaff7e13
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2025-08-29 06:52:56 +0900

    [add]エネミーの行動アニメーション

--- Assets/Scripts/Objects/Enemy.cs
+++ Assets/Scripts/Objects/Enemy.cs
@@ -7,8 +7,10 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField, Range(0, 2)] public int _Lv;
     [SerializeField] private GameObject _marker;
+    [SerializeField] private GameObject _player;
     [SerializeField] private float _gravity = 9.8f;
     [SerializeField] private float _speed = 3f;
+    [SerializeField] private Animator _animator;
     [Header("返球率")]
     [Header("Lv.1"), SerializeField] private float _successRate1 = 50f;
     [Header("Lv.2"), SerializeField] private float _successRate2 = 70f;
@@ -52,10 +54,12 @@ public class Enemy : MonoBehaviour
             if (distance > 0.03f) // 0.1m以上離れてたら移動
             {
                 _move = (targetPos - currentPos).normalized * _speed;
+                _animator.SetFloat("Speed", 0.5f);
             }
             else
             {
                 _move = Vector3.zero; // 到着したら停止
+                _animator.SetFloat("Speed", 0f);
             }
         }
         else
@@ -68,15 +72,30 @@ public class Enemy : MonoBehaviour
             if (distance > 0.03f) // 近くなったら止める
             {
                 _move = (targetPos - currentPos).normalized * _speed / 2f;
+                _animator.SetFloat("Speed", 0.5f);
             }
             else
             {
                 _move = Vector3.zero;
+                _animator.SetFloat("Speed", 0f);
             }
         }
         Vector3 finalMove = _move + new Vector3(0, _velocity.y ,0);
         _CC.Move(finalMove * Time.deltaTime);
-        
+        if(_move.magnitude > 1)
+        {
+            _move = _move.normalized;
+        }
+        if (_move.magnitude > 0.01f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(_move);
+            _tr.rotation = Quaternion.Slerp(_tr.rotation, targetRotation, Time.deltaTime * 10f);
+        }
+        if (_move.magnitude == 0)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(_player.transform.position);
+            _tr.rotation = Quaternion.Slerp(_tr.rotation, targetRotation, Time.deltaTime * 20f);
+        }
     }
     /// <summary>
     /// エネミーの返球率の確率演算
@@ -120,5 +139,14 @@ public class Enemy : MonoBehaviour
         {
             ChaseMode = true;
         }
+    }
+    private void OnAnimatorMove()
+    {
+
+    }
+    private void AlertObservers(string message)
+    {
+        Debug.Log($"AnimationEvent: {message}");
+        // 必要ならここで処理を書く
     }
 }
