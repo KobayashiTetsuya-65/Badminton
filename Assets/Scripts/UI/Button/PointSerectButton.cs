bc3abce035bd2fdd0e92b1c42620df2a7fb957e2
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2025-09-03 03:07:21 +0900

    [add]現在のポイント設定を視覚的にわかりやすくした

--- Assets/Scripts/UI/Button/PointSerectButton.cs
+++ Assets/Scripts/UI/Button/PointSerectButton.cs
@@ -1,9 +1,11 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PointSerectButton : MonoBehaviour
 {
     [SerializeField] private Button _button;
+    [SerializeField] private Button[] _others;
     [SerializeField] private AudioSource _AS;
     [SerializeField] private AudioClip _AC;
     [SerializeField,Range(1,21)] private int _point;
@@ -13,6 +15,7 @@ public class PointSerectButton : MonoBehaviour
         _button.onClick.AddListener(push);
         AudioManager.instance.RegisterSource(_AS);
         _AS.volume = AudioManager.instance.MasterVolume;
+        _button.image.color = Color.white;
     }
     private void OnDestroy()
     {
@@ -22,5 +25,11 @@ public class PointSerectButton : MonoBehaviour
     {
         _AS.PlayOneShot(_AC);
         AudioManager.instance.Point = _point;
+        _button.image.color = new Color(1, 1, 0, 1);
+        _button.transform.DOShakeScale(0.3f);
+        foreach(var button in _others)
+        {
+            button.image.color = Color.white;
+        }
     }
 }
